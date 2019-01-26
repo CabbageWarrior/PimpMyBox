0f8321528df3327e3d862f7924cdd2e4b6d802f3
Andrea(Shiny)Voscilla <andrea.voscilla@hotmail.it>
2019-01-26 13:13:23 +0100

    classe base FornitureGameplayObject creata e annesso scriptable con le informazioni dell'oggetto

---
+++ PimpMyBox-UnityProject/Assets/0_ProjectData/Scripts/FornitureGameplayObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FornitureGameplayObject : MonoBehaviour
+{
+    public System.Action<Player> PickUp;
+    public Forniture fornitureInfos;
+    private SpriteRenderer sR;
+
+    Vector2 storageLocation = new Vector2(100f, 100f);
+
+
+    private void Awake()
+    {
+        sR = GetComponent<SpriteRenderer>();
+        sR.sprite = fornitureInfos.onMapView;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>())
+        {
+            if (PickUp != null)
+                PickUp.Invoke(other.GetComponent<Player>());
+
+            DeSpawn();
+        }
+    }
+
+
+    public void Spawn(Transform where)
+    {
+        this.transform.position = where.transform.position;
+    }
+
+    private void DeSpawn()
+    {
+        this.transform.position = storageLocation;
+    }
+
+}
