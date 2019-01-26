d3890dea19b72921d188e1ab1abaca886bd8a4c1
Verza-ROG <feelthecabbage>
2019-01-26 21:07:49 +0100

    Added things.

--- PimpMyBox-UnityProject/Assets/0_ProjectData/Scripts/Player.cs
+++ PimpMyBox-UnityProject/Assets/0_ProjectData/Scripts/Player.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public SpriteRenderer[] spriteRenderers;
+    public Image dashImage;
     [Space]
     public int playerNumber = 1;
 
@@ -10,7 +12,19 @@ public class Player : MonoBehaviour
     public float deadMovementAxis = .19f;
     public float maxSpeed = 10f;
 
-    public float currentSpeed;
+    [Space]
+    public float dashDeadAmount = .7f;
+
+    public float dashFillTime = 2.5f;
+    public float dashCooldownTime = 5f;
+    public float dashSpeedMaxImpulse = 25;
+
+    private float currentFillTime = 0f;
+    private float currentCooldownTime = 0f;
+
+    private bool canChargeDash = true;
+
+    private bool isDashing = false;
 
     private Rigidbody2D rb2D;
 
@@ -33,17 +47,72 @@ public class Player : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal-P" + playerNumber.ToString());
         float yAxis = Input.GetAxis("Vertical-P" + playerNumber.ToString());
 
+        float rTriggerAxis = Input.GetAxis("RightTrigger-P" + playerNumber.ToString());
+
+        float impulseAmount = 0f;
+
+        if (rTriggerAxis > dashDeadAmount)
+        {
+            // Thumb in Switch position.
+            if (canChargeDash)
+            {
+                currentFillTime += Time.deltaTime;
+                if (currentFillTime >= dashFillTime)
+                {
+                    currentFillTime = dashFillTime;
+                    canChargeDash = false;
+                    isDashing = true;
+
+                    impulseAmount = dashSpeedMaxImpulse;
+                    currentFillTime = 0;
+                    dashImage.fillAmount = 0;
+                }
+                else
+                {
+                    dashImage.fillAmount = currentFillTime / dashFillTime;
+                }
+            }
+        }
+        else
+        {
+            // Thumb in Relax position.
+            if (canChargeDash && currentFillTime > 0f)
+            {
+                canChargeDash = false;
+                isDashing = true;
+
+                impulseAmount = dashSpeedMaxImpulse * currentFillTime / dashFillTime;
+                currentFillTime = 0;
+                dashImage.fillAmount = 0;
+            }
+        }
+
         if (Mathf.Abs(xAxis) > deadMovementAxis || Mathf.Abs(yAxis) > deadMovementAxis)
         {
             float rot_z = Mathf.Atan2(-yAxis, xAxis) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
         }
 
-        rb2D.AddForce(new Vector2(transform.right.x, transform.right.y).normalized * movementSpeedMultiplier);
-        if (rb2D.velocity.sqrMagnitude >= maxSpeed * maxSpeed)
-            rb2D.velocity = rb2D.velocity.normalized * maxSpeed;
-
-        currentSpeed = rb2D.velocity.magnitude;
+        if (!canChargeDash)
+        {
+            if (isDashing)
+            {
+                isDashing = false;
+                rb2D.AddForce(new Vector2(transform.right.x, transform.right.y).normalized * impulseAmount, ForceMode2D.Impulse);
+            }
+            currentCooldownTime += Time.deltaTime;
+            if (currentCooldownTime >= dashCooldownTime)
+            {
+                canChargeDash = true;
+                currentCooldownTime = 0;
+            }
+        }
+        else
+        {
+            rb2D.AddForce(new Vector2(transform.right.x, transform.right.y).normalized * movementSpeedMultiplier);
+            if (rb2D.velocity.sqrMagnitude >= maxSpeed * maxSpeed)
+                rb2D.velocity = rb2D.velocity.normalized * maxSpeed;
+        }
     }
 
     #region Collisions
@@ -63,7 +132,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Dump")
+        if (collision.tag == "Dump")
         {
             bool buttonA = Input.GetButtonDown("AButton-P" + playerNumber.ToString());
             bool buttonB = Input.GetButtonDown("BButton-P" + playerNumber.ToString());
